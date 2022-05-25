001a758c5d791cebb54a55923ed3536afc63a849
Manuel Bieri <manuel.bieri@outlook.com>
2022-05-25 21:19:25 +0200

    Initial commit

---
+++ fmt20_app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from Fumagalli_Motta_Tarantino_2020 import Models, Visualize
+
+st.set_page_config(
+     page_title="Ex-stream-ly Cool App",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get help': 'https://manuelbieri.ch/Fumagalli_2020',
+         'About': "https://github.com/manuelbieri/Fumagalli_2020",
+     }
+ )
+
+
+"""
+## Implementation of Fumagalli et al. (2020)
+"""
+
+m = Models.OptimalMergerPolicy()
+v1 = Visualize.Timeline(m)
+v2 = Visualize.AssetRange(m)
+v3 = Visualize.MergerPoliciesAssetRange(m)
+
+v1.plot()
+v2.plot()
+v3.plot()
+st.pyplot(v1.fig)
+st.pyplot(v2.fig)
+st.pyplot(v3.fig)
