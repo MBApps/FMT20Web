52ad90d4dcab03d173f752190a509dee2c444262
Manuel Bieri <manuel.bieri@outlook.com>
2022-05-29 18:07:59 +0200

    Update FMT20 version and add sliders for additional parameters.

--- fmt20_app.py
+++ fmt20_app.py
@@ -1,6 +1,11 @@
 import sys
 import streamlit as st
+
+from utilities import *
 from Fumagalli_Motta_Tarantino_2020 import Models, Visualize
+
+
+sys.tracebacklimit = 0
 
 st.set_page_config(
      page_title="Home",
@@ -11,23 +16,40 @@ st.set_page_config(
      }
  )
 
-"## Implementation of Fumagalli et al. (2020)"
-sys.tracebacklimit = 0
-p = st.slider("P", 0.0, 1.0, 0.7, 0.01)
+"""
+## Implementation of Fumagalli et al. (2020)
+
+Set the parameters of the model on the left side.
+"""
+
+with st.sidebar:
+    "#### Set the model parameters below"
+    K = create_slider("Development Costs", 'development_costs')
+    A = create_slider("Start-up Assets", 'startup_assets')
+    p = create_slider("Probability for Development Success", 'success_probability')
+    B = create_slider("Private Benefit", 'private_benefit')
+    development_success = st.selectbox('Is Development successful if attempted?',(True, False))
 try:
-    m = Models.OptimalMergerPolicy(success_probability=p)
+    m = Models.OptimalMergerPolicy(
+        development_costs=K,
+        startup_assets=A,
+        success_probability=p,
+        private_benefit=B,
+        development_success=development_success
+    )
     v1 = Visualize.Timeline(m)
     v2 = Visualize.AssetRange(m)
     v3 = Visualize.MergerPoliciesAssetRange(m)
-
     v1.plot()
     v2.plot()
     v3.plot()
-    st.pyplot(v1.fig)
-    st.pyplot(v2.fig)
-    st.pyplot(v3.fig)
+    st.pyplot(v1.fig, transparent=True)
+    st.pyplot(v2.fig, transparent=True)
+    st.pyplot(v3.fig, transparent=True)
 except AssertionError as e:
     st.write(e)
+    with st.expander("Show assumption for debugging"):
+        "These are the assumptions made in the model."
 
 
 
---
+++ utilities.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+import Fumagalli_Motta_Tarantino_2020 as FMT20
+
+
+def create_slider(label: str, param: str):
+    return st.slider(label, 0.0, 1.0, get_default_value(param), 0.01)
+
+def get_default_value(param: str, model=FMT20.BaseModel):
+    args_name = model.__init__.__code__.co_varnames[1:]  # "self" is not needed
+    default_value = model.__init__.__defaults__
+    arg_index = args_name.index(f"{param}")
+    return default_value[arg_index]
