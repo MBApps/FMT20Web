47c6ae457a077e60f5c66ac9058e213971ab676a
Manuel Bieri <manuel.bieri@outlook.com>
2022-05-29 18:25:55 +0200

    Add tooltips.

--- fmt20_app.py
+++ fmt20_app.py
@@ -24,11 +24,13 @@ Set the parameters of the model on the left side.
 
 with st.sidebar:
     "#### Set the model parameters below"
-    K = create_slider("Development Costs", 'development_costs')
-    A = create_slider("Start-up Assets", 'startup_assets')
-    p = create_slider("Probability for Development Success", 'success_probability')
-    B = create_slider("Private Benefit", 'private_benefit')
+    K = create_slider("Development Costs", 'development_costs', 'Costs for the development of the product (independent of success)')
+    A = create_slider("Start-up Assets", 'startup_assets', 'Assets of the start-up at the beginning')
+    p = create_slider("Probability for Development Success", 'success_probability', 'Probability, that the development by the owner of the product is successful')
+    B = create_slider("Private Benefit", 'private_benefit', 'Private benefit of the entrepreneur')
     development_success = st.selectbox('Is Development successful if attempted?',(True, False))
+    show_legend = st.checkbox('Show legends', value=True)
+
 try:
     m = Models.OptimalMergerPolicy(
         development_costs=K,
@@ -40,16 +42,13 @@ try:
     v1 = Visualize.Timeline(m)
     v2 = Visualize.AssetRange(m)
     v3 = Visualize.MergerPoliciesAssetRange(m)
-    v1.plot()
-    v2.plot()
-    v3.plot()
+    v1.plot(legend=show_legend)
+    v2.plot(legend=show_legend)
+    v3.plot(legend=show_legend)
     st.pyplot(v1.fig, transparent=True)
     st.pyplot(v2.fig, transparent=True)
     st.pyplot(v3.fig, transparent=True)
 except AssertionError as e:
     st.write(e)
-    with st.expander("Show assumption for debugging"):
-        "These are the assumptions made in the model."
-
-
-
+    with st.expander("Help for Debugging"):
+        st.markdown('Visit this [link](https://manuelbieri.ch/Fumagalli_2020/Fumagalli_Motta_Tarantino_2020/Models.html#BaseModel.__init__) to learn about the necessary assumptions.')
--- utilities.py
+++ utilities.py
@@ -3,8 +3,8 @@ import streamlit as st
 import Fumagalli_Motta_Tarantino_2020 as FMT20
 
 
-def create_slider(label: str, param: str):
-    return st.slider(label, 0.0, 1.0, get_default_value(param), 0.01)
+def create_slider(label: str, param: str, help_label: str = ''):
+    return st.slider(label, 0.0, 1.0, get_default_value(param), 0.01, help=help_label)
 
 def get_default_value(param: str, model=FMT20.BaseModel):
     args_name = model.__init__.__code__.co_varnames[1:]  # "self" is not needed
