b0093b3e1db43a816361232e5c80a9ab0bcb6903
Manuel Bieri <mail@manuelbieri.ch>
2022-07-11 22:56:13 +0200

    Update app.

--- fmt20_app.py
+++ fmt20_app.py
@@ -35,26 +35,27 @@ def set_config():
 
 
 with st.sidebar:
-    model = st.selectbox('', (FMT20.OptimalMergerPolicy, FMT20.ProCompetitive, FMT20.ResourceWaste), format_func=lambda x:model_label(x), key='model')
+    model = st.selectbox('Select the Model', (FMT20.OptimalMergerPolicy, FMT20.ProCompetitive, FMT20.ResourceWaste), format_func=lambda x:model_label(x), key='model')
     "### Set the model parameters"
     policy = st.selectbox('Merger Policy', (FMT20.MergerPolicies.Strict, FMT20.MergerPolicies.Intermediate_late_takeover_prohibited, FMT20.MergerPolicies.Intermediate_late_takeover_allowed, FMT20.MergerPolicies.Laissez_faire), key='policy')
-    K = create_slider("Development Costs", 'development_costs', 'Costs for the development of the product (independent of success)')
-    A = create_slider("Start-up Assets", 'startup_assets', 'Assets of the start-up at the beginning')
-    p = create_slider("Probability for Development Success", 'success_probability', 'Probability, that the development by the owner of the product is successful')
-    B = create_slider("Private Benefit", 'private_benefit', 'Private benefit of the entrepreneur')
+    K = create_slider("Development Costs", 'development_costs')
+    A = create_slider("Start-up Assets", 'startup_assets')
+    p = create_slider("Probability for Development Success", 'success_probability')
+    B = create_slider("Private Benefit", 'private_benefit')
     development_success = st.selectbox('Is Development successful if attempted?',(True, False), key='development_success')
-    csm = create_slider("Consumer Surplus without Innovation", 'consumer_surplus_without_innovation', '')
-    csM = create_slider("Consumer Surplus with Innovation", 'consumer_surplus_with_innovation', '')
-    csd = create_slider("Consumer Surplus Duopoly", 'consumer_surplus_duopoly', '')
-    pim = create_slider("Incumbent Profit without Innovation", 'incumbent_profit_without_innovation', '')
-    piM = create_slider("Incumbent Profit with Innovation", 'incumbent_profit_with_innovation', '')
-    piId = create_slider("Incumbent Profit Duopoly", 'incumbent_profit_duopoly', '')
-    piSd = create_slider("Start-up Profit Duopoly", 'startup_profit_duopoly', '')
-    gamma = create_slider("Substitutability", 'gamma', '')
+    csm = create_slider("Consumer Surplus without Innovation", 'cs_without_innovation', 'consumer_surplus_without_innovation')
+    csM = create_slider("Consumer Surplus with Innovation", 'cs_with_innovation', 'consumer_surplus_with_innovation')
+    csd = create_slider("Consumer Surplus Duopoly", 'cs_duopoly', 'consumer_surplus_duopoly')
+    pim = create_slider("Incumbent Profit without Innovation", 'incumbent_profit_without_innovation')
+    piM = create_slider("Incumbent Profit with Innovation", 'incumbent_profit_with_innovation')
+    piId = create_slider("Incumbent Profit Duopoly", 'incumbent_profit_duopoly')
+    piSd = create_slider("Start-up Profit Duopoly", 'startup_profit_duopoly')
+    gamma = create_slider("Substitutability", 'gamma')
     configs = st.selectbox('Available configurations',get_configurations(), format_func=lambda x:configuration_label(x), on_change=set_config, key='configs')
 
 
 try:
+    st.markdown("<h2 style='text-align: center;'>Model Overview</h2>", unsafe_allow_html=True)
     m = st.session_state.model(
         merger_policy=st.session_state.policy,
         development_costs=st.session_state.development_costs,
@@ -62,9 +63,9 @@ try:
         success_probability=st.session_state.success_probability,
         private_benefit=st.session_state.private_benefit,
         development_success=st.session_state.development_success,
-        consumer_surplus_without_innovation=st.session_state.consumer_surplus_without_innovation,
-        consumer_surplus_with_innovation=st.session_state.consumer_surplus_with_innovation,
-        consumer_surplus_duopoly=st.session_state.consumer_surplus_duopoly,
+        consumer_surplus_without_innovation=st.session_state.cs_without_innovation,
+        consumer_surplus_with_innovation=st.session_state.cs_with_innovation,
+        consumer_surplus_duopoly=st.session_state.cs_duopoly,
         incumbent_profit_without_innovation = st.session_state.incumbent_profit_without_innovation,
         incumbent_profit_duopoly = st.session_state.incumbent_profit_duopoly,
         startup_profit_duopoly = st.session_state.startup_profit_duopoly,
@@ -72,7 +73,7 @@ try:
         asset_distribution=FMT20.Distributions.UniformDistribution
     )
     v = FMT20.Overview(m, default_style=False, figsize=(9.5, 7))
-    v.plot(fontsize=7, title='', thresholds=True, optimal_policy=True, y_offset=-40)
+    v.plot(fontsize=9, figure_title='', title='', thresholds=True, optimal_policy=True, y_offset=-40)
     v.fig.set_label("Fumagalli et al. (2020)")
     st.pyplot(v.fig, transparent=True, dpi=750)
 except AssertionError as e:
